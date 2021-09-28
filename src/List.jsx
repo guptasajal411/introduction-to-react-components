abf93653e288318fbdfb3a1d690fa447f39f6ee6
Sajal Gupta <guptasajal411@gmail.com>
2021-09-28 20:41:48 +0530

    Create new List component with JSX file

---
+++ src/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function List(){
+    return <ul>
+        <li>Full Stack Web Developer</li>
+        <li>Web Lead @ GeeksforGeeks VIT Bhopal</li>
+        <li>CS Sophomore @ VIT Bhopal</li>
+    </ul>
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Heading from './Heading'
+import Heading from './Heading';
+import List from './List';
 
 ReactDOM.render(
     <div>
         <Heading />
-        <ul>
-            <li>Full Stack Web Developer</li>
-            <li>Web Lead @ GeeksforGeeks VIT Bhopal</li>
-            <li>CS Sophomore @ VIT Bhopal</li>
-        </ul>
+        <List />
     </div>,
     document.querySelector("#root")
 );
