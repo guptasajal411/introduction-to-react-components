b7cc40933a5a0942e68c8d935b0e533dd5e8fc7a
Sajal Gupta <guptasajal411@gmail.com>
2021-09-28 20:32:18 +0530

    Add content with ReactDOM.render() method

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ var ReactDOM = require('react-dom');
 
 ReactDOM.render(
     <div>
-        <h1>Hello World!</h1>
+        <h1>Hello, I am Sajal!</h1>
+        <ul>
+            <li>Full Stack Web Developer</li>
+            <li>Web Lead @ GeeksforGeeks VIT Bhopal</li>
+            <li>CS Sophomore @ VIT Bhopal</li>
+        </ul>
     </div>,
-    document.querySelector("#root"));
+    document.querySelector("#root")
+);
