6461e7afe5f3e20fc5a309390325d6679a129f5a
Sajal Gupta <guptasajal411@gmail.com>
2021-09-30 10:44:17 +0530

    Use map function to render array of contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,28 @@ import Heading from './Heading';
 import Time from './Time';
 import List from './List';
 import Card from './Card';
+import Avatar from './Avatar';
 import "./styles.css"
+
+function createCard(contact){
+    return(
+        <Card
+            name={contact.name}
+            imgURL={contact.imgURL}
+            phone={contact.phone}
+            email={contact.email}
+        />
+    );
+}
 
 function App(){
     return(
         <div>
+            <Avatar imgURL="https://github.com/guptasajal411.png" />
             <Heading />
             <Time />
             <List />
-            <Card name={contacts[0].name} imgURL={contacts[0].imgURL} phone={contacts[0].phone} email={contacts[0].email} />
-            <Card name={contacts[1].name} imgURL={contacts[1].imgURL} phone={contacts[1].phone} email={contacts[1].email} />
-            <Card name={contacts[2].name} imgURL={contacts[2].imgURL} phone={contacts[2].phone} email={contacts[2].email} />
+            {contacts.map(createCard)}
         </div>
     );
 }
